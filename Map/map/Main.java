a227f342351856ca55aafb04a8768841b1d31f1b
Roman <romanbr@meta.ua>
2018-11-18 17:31:27 +0200

    Update Main.java

--- Map/map/Main.java
+++ Map/map/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 	//	HashMap<String, Integer> myHashMap = new HashMap<String, Integer>();
 		HashMap<String, String> myHashMap = new HashMap<String, String>();
-		myHashMap.put("Basil", "07.12.1987");											//put
+		myHashMap.put("Basil", "07.12.1987");							//put
 		myHashMap.put("Kate", "12.10.1971");
 		myHashMap.put("Lena", "11.01.1991");
 		for (Entry<String, String> entry : myHashMap.entrySet())
@@ -19,7 +19,7 @@ public class Main {
 		
 		
 
-        myHashMap.remove("Kate");														//remove
+        myHashMap.remove("Kate");									//remove
         System.out.println("\nAfter removing the key:");
         for(Map.Entry<String, String> entry: myHashMap.entrySet())
             System.out.println(entry.getKey() + " - " + entry.getValue());
